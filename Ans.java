1cfa2c05d71c74abd604736db43e241e027fa258
vivek singh <55220203+ui-vivek@users.noreply.github.com>
2023-03-16 10:06:47 +0530

    Update Ans.java

--- Ans.java
+++ Ans.java
@@ -1,4 +1,17 @@
+// Sir/Mam,
 
+// Before checking my code, I humble request you to please read this.
+// The question you have given to me is 
+
+// print the longest similar characters sequentially in both the strings.
+// Let str1 = "ALHIOPK" Let str2 = "KHXOLPMK
+// Output: "KHOPK"
+// the expected output is incorrect because it is clearly noticeable that "L" is missing from the output.  "L" is in the string 1 and string 2 also, so the expected output would be "KHOLPK".
+
+// I have made the code for the correct output, so look at the problem please.
+
+// I hope my you are impressed with my solutions with different types of approaches.
+// -----------------------------------------------------------------------------------------------
 class HelloWorld {
     public static String longAns(String str2, String str1){
         String longSub="";
